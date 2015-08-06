1791a66c9488931101ab7b2832d67928c74c6802
ggkk0818 <ggkk0818@sina.com>
2015-08-06 18:09:15 +0800

    解决左侧二级菜单错误的问题

---
+++ zegna/js/ieUpdate.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    var html = '<div id="ieUpdateDiv" style="position:absolute;top:-42px;left:50%;margin-left:-410px;width:820px;height:42px;z-index:2;">'
+        + '<a href="http://windows.microsoft.com/zh-cn/internet-explorer/download-ie" target="_blank"><img alt="" src="img/warning_bar_0027_Simplified Chinese.jpg" style="border:0;" /></a>'
+        + '</div>';
+    document.body.innerHTML += html;
+    $("#ieUpdateDiv").delay(1000).animate({ top: 0 }, "slow").delay(10000).animate({ top: -42 }, "slow", function () { $(this).remove(); });
+});
